98de6f6c10cfc3d5f29ece7b92c098747aaf1c35
zyc <zycet@126.com>
2016-04-27 09:29:52 +0800

    添加FlashJob类型

---
+++ FlasherLib/FlashJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BUAA
+{
+    public enum JobTypeEnum
+    {
+
+    }
+
+    public class FlashJob
+    {
+        
+    }
+}
